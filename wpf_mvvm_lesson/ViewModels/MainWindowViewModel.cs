0edfdd0e341f75a509c70d845895b439fc44348b
kkirrill <kkirrill1999@gmail.com>
2021-08-22 16:46:45 +0700

    Тестовые работы модели представления

---
+++ wpf_mvvm_lesson/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpf_mvvm_lesson.ViewModels.Base;
+
+namespace wpf_mvvm_lesson.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    { 
+        private string _Title = "Анализ статистики CV19";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+            /*{
+                //if (Equals(_Title, value)) return;
+                //_Title = value;
+                //OnPropertyChanged();
+                Set(ref _Title, value);
+            } */
+        }
+    }
+}
