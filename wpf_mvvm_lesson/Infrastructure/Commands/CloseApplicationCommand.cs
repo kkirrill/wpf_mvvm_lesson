959fb23d748ca725c015cf57751ff536323bdd84
kkirrill <kkirrill1999@gmail.com>
2021-08-25 16:26:15 +0700

    Добавление fontawesome5 в главный проект и Material Design в тестовый Wpf проект

---
+++ wpf_mvvm_lesson/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using wpf_mvvm_lesson.Infrastructure.Commands.Base;
+
+namespace wpf_mvvm_lesson.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
