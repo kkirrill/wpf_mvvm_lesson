c85967eb2404b5315bb7489dfa469703eb4dc10b
kkirrill <kkirrill1999@gmail.com>
2021-08-25 17:19:48 +0700

    Запрос к удаленному серверу для получения серых данных

--- AppConsoleTest/Program.cs
+++ AppConsoleTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,16 @@ namespace AppConsoleTest
 {
     class Program
     {
+        private const string url_data = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
         static void Main(string[] args)
         {
+
+            var client = new HttpClient();
+
+            var response = client.GetAsync(url_data).Result;
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+            Console.ReadLine();
+
         }
     }
 }
