c767860fd4a10b5d38604c6ac88c07b6353293f5
kkirrill <kkirrill1999@gmail.com>
2021-10-04 15:09:59 +0700

    Комманды создания и удаления элементов из списков данных модели-представления

--- wpf_mvvm_lesson/ViewModels/MainWindowViewModel.cs
+++ wpf_mvvm_lesson/ViewModels/MainWindowViewModel.cs
@@ -99,6 +99,42 @@ namespace wpf_mvvm_lesson.ViewModels
 
 
         #endregion
+
+        # region CreateGroupCommand
+
+        public ICommand CreateGroupCommand { get; }
+        private bool CanCreateGroupCommandExecute(object p) => true;
+
+        private void OnCreateGroupCommandExecuted(object p)
+        {
+            var group_max_index = Groups.Count + 1;
+            var new_group = new Group
+            {
+                Name = $"Гурппа {group_max_index}",
+                Students = new ObservableCollection<Student>()
+            };
+
+            Groups.Add(new_group);
+        }
+
+        #endregion
+
+        #region DeleteGroupCommand
+
+        public ICommand DeleteGroupCommand { get; }
+        private bool CanDeleteGroupCommandExecute(object p) => p is Group group && Groups.Contains(group);
+        private void OnDeleteGroupCommandExecuted(object p)
+        {
+            if (!(p is Group group)) return;
+            var group_index = Groups.IndexOf(group);
+            Groups.Remove(group);
+            if (group_index < Groups.Count)
+                SelectedGroup = Groups[group_index];
+            
+        }
+
+        #endregion
+
         #endregion
 
         /*-------------------------------------------------------------------------------------*/
@@ -108,6 +144,8 @@ namespace wpf_mvvm_lesson.ViewModels
             #region Команды
 
             CloseApplicationCommand = new LamdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+            CreateGroupCommand = new LamdaCommand(OnCreateGroupCommandExecuted, CanCreateGroupCommandExecute);
+            DeleteGroupCommand = new LamdaCommand(OnDeleteGroupCommandExecuted, CanDeleteGroupCommandExecute);
 
             #endregion
 
